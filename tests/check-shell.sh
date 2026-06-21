843119d133eb5531519d36d2bbdab75f4c4578cf
timogilvie <me@timogilvie.com>
2026-06-21 12:39:28 -0400

    fix: ignore local timer vars in shell guard

--- tests/check-shell.sh
+++ tests/check-shell.sh
@@ -331,7 +331,7 @@ else
       | grep -vE '^(bad|internal|marking|rate|reduce|service|timed|too|using|wavemill|waiting)$' \
       | grep -vE '^(advance|review)$' \
       | grep -vE '^(not_eligible|routing_error)$' \
-      | grep -vE '^(a|aborted|already|available|blocked_by_count|break|coding|cp|debug|empty_queue|execute|file|fresh|gtimeout|id|launch|length|main|mapfile|missing|not|overloaded|plan|ready|required|reservation|slots|the|they|timeout|todate|todateiso8601|tonumber|tracked|user)$')
+      | grep -vE '^(a|aborted|already|available|blocked_by_count|break|coding|cp|debug|empty_queue|execute|file|fresh|gtimeout|id|last_run|launch|length|main|mapfile|missing|not|now|overloaded|plan|ready|required|reservation|slots|the|they|timeout|todate|todateiso8601|tonumber|tracked|user)$')
 
     # Check which called names look like they could be custom functions
     # and verify they're defined
